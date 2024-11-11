ad46d4b9eb4256c1d3e5d7e907e3419bde6caeb7
Ayi <erictim1217@gmail.com>
2024-11-11 16:40:38 +0800

    update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -115,11 +115,12 @@ async function syncTableWithApi() {
       await context.sync();
 
       // 逐行處理 API 資料
-      workbookData.forEach((item) => {
+      workbookData.forEach(async (item) => {
         const id = item.id;
         const range = sheet.getRange("A2:A1000");
         // TODO:
         sheet.load("values");
+        await sheet.context.sync();
         console.log(range);
         // item.items.forEach(async (field) => {
         // 根據編號和項目名稱找到儲存格並填充值
