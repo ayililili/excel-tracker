e583a07b2913271826764a2a038c50087a711574
Ayi <erictim1217@gmail.com>
2024-11-13 11:18:31 +0800

    update excel.service.js

--- src/services/excel.service.js
+++ src/services/excel.service.js
@@ -1,12 +1,14 @@
 export class ExcelService {
   async getWorkbookName() {
     try {
+      let workbookName;
       await Excel.run(async (context) => {
         const workbook = context.workbook;
         workbook.load("name");
         await context.sync();
-        return workbook.name;
+        workbookName = workbook.name;
       });
+      return workbookName;
     } catch (error) {
       console.error("無法獲取檔案名：", error);
       throw error;
