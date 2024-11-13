050af303857574de7d92e362782bba41727c093d
Ayi <erictim1217@gmail.com>
2024-11-13 16:33:16 +0800

    update cell-change.handler.js

--- src/handlers/cell-change.handler.js
+++ src/handlers/cell-change.handler.js
@@ -9,7 +9,7 @@ export class CellChangeHandler {
     const [column, row] = changedCell.match(/[A-Z]+|\d+/g);
 
     if (parseInt(row, 10) === 1 || parseInt(column, 10) === 1) {
-      return;
+      return 1;
     }
 
     const idCell = `A${row}`;
