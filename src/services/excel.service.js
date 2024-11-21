f8c75122b6b66174aad82f0b9b68733f0121c430
Ayi <erictim1217@gmail.com>
2024-11-21 13:50:32 +0800

    update excel.service.js

--- src/services/excel.service.js
+++ src/services/excel.service.js
@@ -486,9 +486,9 @@ export class ExcelService {
                 isSync: false,
               };
             }
+            console.log(changes[id].values);
             // 新增專案號
             if (this.documentType === DOCUMENT_TYPES.DEPARTMENT && changes[id]) {
-              console.log(changes[id].values);
               changes[id].values.projectNumber = this.projectNumber;
             }
           }
