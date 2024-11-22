fcfb4238d5cd8492806a45517cd656b8c2382484
Ayi <erictim1217@gmail.com>
2024-11-22 09:23:22 +0800

    update excel.service.js

--- src/services/excel.service.js
+++ src/services/excel.service.js
@@ -421,7 +421,10 @@ export class ExcelService {
         // 跳過標題行，從第二行開始
         for (let row = 1; row < idValues.length; row++) {
           let id = idValues[row][0];
-          const rowHasData = Object.values(columnHeaders).some((_, index) => ranges[index].values[row][0] !== "");
+          const rowHasData = Object.values(columnHeaders).some((_, index) => {
+            console.log(index);
+            ranges[index].values[row][0] !== "";
+          });
 
           // 處理空白ID的情況
           if (!id && this.documentType === DOCUMENT_TYPES.DEPARTMENT && rowHasData) {
